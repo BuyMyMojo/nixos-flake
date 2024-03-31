549e300076ec5fbb7156492a130eb2aa74c0148f
BuyMyMojo <hello@buymymojo.net>
2024-03-31 16:52:45 +1100

    Remove 'videoDrivers' option to try to fix XIVLauncher

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -427,8 +427,9 @@
         autoLogin.user = "buymymojo";
       };
 
+      # Disabled for xiv launcher?
       # Load nvidia driver for Xorg and Wayland
-      videoDrivers = [ "nvidia" ]; # or "nvidiaLegacy470 etc.
+      # videoDrivers = [ "nvidia" ]; # or "nvidiaLegacy470 etc.
 
       xkb.layout = "us";
       xkb.variant = "";
